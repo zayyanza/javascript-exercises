0d5981883699fc7ca60083128469555cf67a7699
zetampo <zbanjar9@gmail.com>
2024-08-14 19:52:38 +0800

    solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    result = array.filter(function (element) {
+        return !args.includes(element);
+    })
+    return result;
 };
 
 // Do not edit below this line
