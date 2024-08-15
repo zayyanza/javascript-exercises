6df9c07e8305cd6a6117d2a4f4cac650d1b9031c
zetampo <zbanjar9@gmail.com>
2024-08-16 05:41:14 +0700

    solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  result = (num - 32) * 5/9;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  result = num * 9/5 + 32;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
