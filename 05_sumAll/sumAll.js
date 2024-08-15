980ca9fd85ae5aba9d5cf0e1d72fc54699130308
zetampo <zbanjar9@gmail.com>
2024-08-15 16:27:12 +0800

    solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    } else if (a < 0 || b < 0) {
+        return "ERROR";
+    } else {
+        sum = 0;
+        if (a < b) {
+            for (let i = a; i <= b; i++) {
+                sum += i;
+            }
+        } else if (a > b) {
+            for (let i = a; i >= b; i--) {
+                sum += i;
+            }
+        } else {
+            sum += a;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
