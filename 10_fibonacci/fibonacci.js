46be202efd3da03eecb12909399fbc4af0806f70
zetampo <zbanjar9@gmail.com>
2024-08-16 08:29:04 +0700

    solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let int = parseInt(num);
+    let fibonacci = [];
 
+    if (int === 0) {
+        return 0;
+    }
+
+    if (int < 0) {
+        return "OOPS";
+    }
+
+    for(let i = 0; i < int; i++) {
+        if (fibonacci.length < 2) {
+            fibonacci.push(1);
+        } else {
+            fibonacci.push(fibonacci[fibonacci.length-1] + fibonacci[fibonacci.length-2])
+        }
+    }
+    return fibonacci[fibonacci.length-1];
 };
 
 // Do not edit below this line
