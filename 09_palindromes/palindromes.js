1f100fe891c61f12a7c1dbd4541e4457a59d22a3
zetampo <zbanjar9@gmail.com>
2024-08-16 08:09:44 +0700

    solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    const punctuationArray = ['.', ',', '!', '?', ';', ':', '-', '_',
+         '(', ')', '[', ']', '{', '}', "'", '"', '/', '\\', '|', '@', '#', '$', '%', '^', '&', '*', '+', '=', '<', '>', '`', '~'];
+    let splitted = string.toLowerCase().split("");
+    let filtered = splitted.filter(function (char) {
+        return !punctuationArray.includes(char) && char !== " ";
+    })
+    let reversed = [...filtered].reverse();
+    return filtered.join("") === reversed.join("");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
