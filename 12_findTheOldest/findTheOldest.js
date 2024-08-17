a417235b9ddbb988cdaab54c47568d540f23209d
zetampo <zbanjar9@gmail.com>
2024-08-17 11:44:18 +0800

    solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people.reduce((oldest, person) => {
+        let currentYear = new Date().getFullYear();
+        let ageOfPerson = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        let ageOfOldest = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
 
+        return ageOfPerson > ageOfOldest ? person : oldest;
+    });
+    return oldest;
 };
 
 // Do not edit below this line
